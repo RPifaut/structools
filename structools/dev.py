1a1260e57e0e04fcfdcbd05251e217e343e84c06
Romain Pifaut <romain.pifaut@gmail.com>
2024-12-02 20:22:16 +0100

    feat: Adding the Market class

--- structools/dev.py
+++ structools/dev.py
@@ -4,9 +4,8 @@ from pybacktestchain.data_module import DataModule, get_stocks_data
 from src.structools.products.basic_products import Option, Underlying
 from src.structools.products.autocalls import Phoenix, Athena
 from src.structools.tools.date_tools import DateModel
-from src.structools.tools.market import load_stocks_data
+from src.structools.tools.market import Market
 
-output = load_stocks_data(["AAPL", "MSFT"], DateModel(date="2000-01-01"), DateModel(date="2024-01-01"))
+output = Market.create_market(["AAPL", "MSFT"], DateModel(date="2000-01-01"), DateModel(date="2024-01-01"))
 
 print(output)
-print(output.index, output.index.dtype, type(output.index))
--- structools/src/structools/tools/market.py
+++ structools/src/structools/tools/market.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Dict
 from src.structools.tools.date_tools import DateModel
 from pybacktestchain.data_module import DataModule, get_stocks_data
+
+L_PRICES = ["Open", "High", "Low", "Close", "Adj. Close"]
 
 
 
@@ -41,3 +43,64 @@ def load_stocks_data(tickers : List[str], start_date : DateModel, end_date : Dat
     )
 
     return df_output
+
+
+class Market(BaseModel):
+
+    """
+    This class will be used for the backtest of strategies. It aims at preventing the use of out of sample date in estimations.
+    """
+
+    class Config:
+
+        arbitrary_types_allowed = True
+
+    data : Dict[str, pd.DataFrame]
+
+
+    @classmethod
+    def create_market(cls, tickers : List[str], start_date : DateModel, end_date : DateModel, uniform : bool = True):
+
+        """
+        Class method to create an instance of market.
+
+        Parameters:
+
+            tickers (List[str]): List of the tickers of companies composing the market
+            start_date (DateModel): Date from which we start loading the data
+            end_date (DateModel): Date at which we stop loading the data
+            uniform (bool): Boolean to indicate whether we only keep days where there is quotation for all components. Default is True
+
+        Returns:
+
+            Instance of market
+
+        """
+
+        dict_data = {}
+
+        # Load the market data
+        df_data = load_stocks_data(tickers, start_date, end_date)
+
+        # Break the output down by ticker and store them in the dictionary
+        for ticker in tickers:
+            dict_data.update(
+                {
+                    ticker: df_data[df_data["ticker"]==ticker]
+                }
+            )
+
+        # If set to True, create a uniform index
+        if uniform:
+            
+            # Create Uniform index
+            my_index = df_data[df_data["ticker"]==tickers[0]].index
+            for ticker in tickers:
+                my_index = np.intersect1d(my_index, dict_data[ticker].index)
+
+            # Modify
+            my_index = pd.DataFrame(index = my_index)
+            for ticker in tickers:
+                dict_data[ticker] = pd.concat([my_index, dict_data[ticker]], axis=1, join='inner')
+
+        return cls(data = dict_data)
